1feac793019adf5b481220daada306541221148b
nikoladomazetovikj <nikolad150@gmail.com>
2022-12-01 11:16:41 +0100

    initial commit

---
+++ src/main/java/com/seeu/java/traveling_is_fun/TravelingIsFunApplication.java
@@ -0,0 +1,13 @@
+package com.seeu.java.traveling_is_fun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelingIsFunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelingIsFunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seeu/java/traveling_is_fun/TravelingIsFunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seeu.java.traveling_is_fun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelingIsFunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
