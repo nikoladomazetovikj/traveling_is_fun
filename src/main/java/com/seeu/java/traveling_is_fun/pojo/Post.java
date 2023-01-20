66e48e2c677cfd163aa6cfa52ab484a800dea334
bilbilovska99 <ab29303@seeu.edu.mk>
2023-01-20 19:54:02 +0100

    feat:created post entity

--- src/main/java/com/seeu/java/traveling_is_fun/pojo/Post.java
+++ src/main/java/com/seeu/java/traveling_is_fun/pojo/Post.java
@@ -1,4 +1,86 @@
 package com.seeu.java.traveling_is_fun.pojo;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "post")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="title")
+
+    private String title;
+    @Column(name="content")
+    private String content;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private User author;
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+    private List<Comment> comments;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+    private List<Like> likes;
+
+    public Post() {
+    }
+
+    public Post(Long id, String title, String content, User author, List<Comment> comments, List<Like> likes) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.comments = comments;
+        this.likes = likes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public List<Like> getLikes() {
+        return likes;
+    }
+
+    public void setLikes(List<Like> likes) {
+        this.likes = likes;
+    }
 }
+
