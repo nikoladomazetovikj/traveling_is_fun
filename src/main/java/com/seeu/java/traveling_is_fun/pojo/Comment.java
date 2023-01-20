2957bdea4b9f72321f30b59039d412e7957cdf54
bilbilovska99 <ab29303@seeu.edu.mk>
2023-01-20 20:00:12 +0100

    feat:created comment entity

---
+++ src/main/java/com/seeu/java/traveling_is_fun/pojo/Comment.java
@@ -0,0 +1,62 @@
+package com.seeu.java.traveling_is_fun.pojo;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "comment")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "text")
+    private String text;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Comment() {
+    }
+
+    public Comment(Long id, String text, Post post, User user) {
+        this.id = id;
+        this.text = text;
+        this.post = post;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
