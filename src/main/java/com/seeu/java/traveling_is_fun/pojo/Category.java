104214fafb2c4534ffd05c2e96bbe3aa79f3a543
nikoladomazetovikj <nikolad150@gmail.com>
2023-01-21 15:11:02 +0100

    feat: added categories table

---
+++ src/main/java/com/seeu/java/traveling_is_fun/pojo/Category.java
@@ -0,0 +1,15 @@
+package com.seeu.java.traveling_is_fun.pojo;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+}
