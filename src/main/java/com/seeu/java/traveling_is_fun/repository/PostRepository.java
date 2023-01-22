732c294ed58f3923f0ef973460768418608dfcde
bilbilovska99 <ab29303@seeu.edu.mk>
2023-01-22 18:02:46 +0100

    feat: created Post repository

--- src/main/java/com/seeu/java/traveling_is_fun/repository/PostRepository.java
+++ src/main/java/com/seeu/java/traveling_is_fun/repository/PostRepository.java
@@ -1,4 +1,24 @@
 package com.seeu.java.traveling_is_fun.repository;
 
-public interface PostRepository {
+import com.seeu.java.traveling_is_fun.pojo.Post;
+import com.seeu.java.traveling_is_fun.pojo.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    void likePost(Long postId, Long userId);
+
+    void commentOnPost(Long postId, Long userId, String text);
+    void createPost(Long userId,String title, String content);
+    void editPost(Long postId, String title, String content);
+    int getNumberOfLikes(Long postId);
+
+    int getNumberOfComments(Long postId);
+    List<User> getLikedUsers(Long postId);
+
+    List<User> getCommentedUsers(Long postId);
+
 }
