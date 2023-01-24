513a2d79c4c71f5e4a2d1d37477655fc1d8ef91f
bilbilovska99 <ab29303@seeu.edu.mk>
2023-01-24 20:32:04 +0100

    feat: edited Post controller

--- src/main/java/com/seeu/java/traveling_is_fun/controller/PostController.java
+++ src/main/java/com/seeu/java/traveling_is_fun/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.seeu.java.traveling_is_fun.controller;
 
+import com.seeu.java.traveling_is_fun.pojo.Category;
 import com.seeu.java.traveling_is_fun.pojo.Comment;
 import com.seeu.java.traveling_is_fun.pojo.Post;
 import com.seeu.java.traveling_is_fun.pojo.User;
@@ -14,13 +15,20 @@ import java.util.List;
 public class PostController {
     private PostService postService;
 
-
+    @GetMapping("/posts/allPosts")
+    public List<Post> getAllPosts() {
+        return postService.getAllPosts();
+    }
+    @GetMapping("posts/{id}")
+    public Post getPostById( @PathVariable Long id){
+        return postService.getPostById(id);
+    }
     @PostMapping("/posts/create")
     public void createPost(@RequestBody Post post) {
         postService.createPost(post.getId(), post.getTitle(), post.getContent(), post.getAuthor(), post.getCategory());
 
     }
-
+ 
 
 
 
