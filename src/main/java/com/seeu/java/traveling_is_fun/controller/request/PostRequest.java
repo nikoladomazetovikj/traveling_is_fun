56f0d5e8e21939093b477b52c0f9ef867e172723
bilbilovska99 <ab29303@seeu.edu.mk>
2023-01-25 23:06:00 +0100

    feat: created PostRequest

---
+++ src/main/java/com/seeu/java/traveling_is_fun/controller/request/PostRequest.java
@@ -0,0 +1,11 @@
+package com.seeu.java.traveling_is_fun.controller.request;
+
+import com.seeu.java.traveling_is_fun.pojo.Category;
+
+public class PostRequest {
+    public String title;
+    public String content;
+    public Long userId;
+    public Long categoryId;
+
+}
