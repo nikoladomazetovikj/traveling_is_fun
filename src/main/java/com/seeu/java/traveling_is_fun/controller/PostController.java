e39e7c11d2230c8ca0d10a923cbb5ea65ca0434a
bilbilovska99 <ab29303@seeu.edu.mk>
2023-01-25 23:07:20 +0100

    feat: updated PostController

--- src/main/java/com/seeu/java/traveling_is_fun/controller/PostController.java
+++ src/main/java/com/seeu/java/traveling_is_fun/controller/PostController.java
@@ -1,36 +1,52 @@
 package com.seeu.java.traveling_is_fun.controller;
 
+import com.seeu.java.traveling_is_fun.controller.request.PostRequest;
 import com.seeu.java.traveling_is_fun.pojo.Category;
-import com.seeu.java.traveling_is_fun.pojo.Comment;
 import com.seeu.java.traveling_is_fun.pojo.Post;
 import com.seeu.java.traveling_is_fun.pojo.User;
 import com.seeu.java.traveling_is_fun.service.PostService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class PostController {
-    private PostService postService;
+    private  final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
 
     @GetMapping("/posts/allPosts")
     public List<Post> getAllPosts() {
         return postService.getAllPosts();
     }
+
     @GetMapping("posts/{id}")
-    public Post getPostById( @PathVariable Long id){
+    public Post getPostById(@PathVariable Long id) {
         return postService.getPostById(id);
     }
+
     @PostMapping("/posts/create")
-    public void createPost(@RequestBody Post post) {
-        postService.createPost(post.getId(), post.getTitle(), post.getContent(), post.getAuthor(), post.getCategory());
+    public void createPost(@RequestBody PostRequest post) {
+        postService.createPost(post.title, post.content, post.userId, post.categoryId);
 
     }
+    @PutMapping("/posts/edit/{id}")
+    public void editPost(@PathVariable Long id,@RequestParam String title,@RequestParam String content,@RequestParam Category category){
+        postService.editPost(id,title,content,category);
+    }
 
+    @DeleteMapping("/posts/delete/{id}")
+    public void deletePost(@PathVariable Long id) {
+        postService.deletePost(id);
+    }
+    @PutMapping("/posts/like/{id}")
+    public void likePost(@PathVariable Long id, @RequestBody User author){
 
-
+    }
 
 
 }
