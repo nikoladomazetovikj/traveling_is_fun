360a270fbeaa7df3e6bb484d0ac76e38daa7154c
bilbilovska99 <ab29303@seeu.edu.mk>
2023-01-26 20:50:22 +0100

    feat:updated PostRequest

--- src/main/java/com/seeu/java/traveling_is_fun/controller/request/PostRequest.java
+++ src/main/java/com/seeu/java/traveling_is_fun/controller/request/PostRequest.java
@@ -1,11 +1,9 @@
 package com.seeu.java.traveling_is_fun.controller.request;
 
-import com.seeu.java.traveling_is_fun.pojo.Category;
-
 public class PostRequest {
     public String title;
     public String content;
     public Long userId;
-    public Long categoryId;
+    public String category;
 
 }
