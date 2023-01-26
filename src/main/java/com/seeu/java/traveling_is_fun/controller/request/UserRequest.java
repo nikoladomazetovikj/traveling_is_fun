df4f538929dda47a43cf5b6c9835d57e14c2c802
nikoladomazetovikj <nikolad150@gmail.com>
2023-01-26 21:22:45 +0100

    feat(user): created request

---
+++ src/main/java/com/seeu/java/traveling_is_fun/controller/request/UserRequest.java
@@ -0,0 +1,14 @@
+package com.seeu.java.traveling_is_fun.controller.request;
+
+import javax.persistence.Column;
+
+public class UserRequest {
+    public String name;
+    public String country;
+    public String surname;
+    public String email;
+    public String userName;
+    public String password;
+    public boolean isAdmin;
+    public boolean isBlogger;
+}
