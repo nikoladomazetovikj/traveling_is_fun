39d28974d4603bfdba3e7014b3cefb599dd636d1
nikoladomazetovikj <nikolad150@gmail.com>
2023-01-26 21:11:12 +0100

    cleanup: remove category repository and service

--- src/main/java/com/seeu/java/traveling_is_fun/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.seeu.java.traveling_is_fun.repository;
-
-import com.seeu.java.traveling_is_fun.pojo.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category,Long> {
-}
